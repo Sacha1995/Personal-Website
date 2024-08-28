747410de58feaf0ca5a66f9b3717120b18e361ad
Sacha Uijlen <sacha-u@hotmail.com>
2024-08-28 20:36:28 +0100

    trying to fix CORS problem on wave

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allows any origin to access the resource
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS", // Define the allowed methods
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Accept",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
